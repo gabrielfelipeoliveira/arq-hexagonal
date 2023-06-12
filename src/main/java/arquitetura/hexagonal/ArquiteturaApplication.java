5afd6ea447ec1b3b4df11c0bfc13fb5a821a530d
Gabriel <gabrielfe.bnu@gmail.com>
2023-06-11 22:32:21 -0300

    Commit inicial

---
+++ src/main/java/arquitetura/hexagonal/ArquiteturaApplication.java
@@ -0,0 +1,13 @@
+package arquitetura.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arquitetura/hexagonal/ArquiteturaApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitetura.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
